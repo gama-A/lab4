370cf5b32d4fb103be1663ebb61b57b2dbe5b94d
Gama Aristondo <gamaliel@cs.ucsb.edu>
2021-02-01 21:15:56 -0800

    figuring out loop

--- factorial.asm
+++ factorial.asm
@@ -1,6 +1,6 @@
 # Factorial.asm program
 # CS 64, Z.Matni, zmatni@ucsb.edu
-#
+# Gama Aristondo
 
 # Assembly (NON-RECURSIVE) version of:
 #   int n, fn=1;
@@ -16,13 +16,53 @@
 	Enter: .asciiz "Enter a number:\n"
     Factorial: .asciiz "Factorial of "
     Answer: .asciiz " is:\n"
+    Newline: .asciiz "\n"
 
 #Text Area (i.e. instructions)
 .text
 main:
 
-	# TODO: Write your code here
+	li $v0, 4
+    la $a0, Enter
+    syscall
+
+    li $v0, 5
+    syscall
+
+    move $t0, $v0
+    li $t1, 1
+    beq $t0, $zero, answer
+    li $t2, 2
+
+factorial:
+
+    beq $t1, $t0, answer
+    mult $t1
+    j factorial
+
+answer:
+
+    li $v0, 4
+    la $a0, Factorial
+    syscall
+
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    li $v0, 4
+    la $a0, Answer
+    syscall
+
+    li $v0, 1
+    move $a0, $t1
+    syscall
+
+    li $v0, 4
+    la $a0, Newline
+    syscall
 
 exit:
+
 	li $v0, 10
     syscall

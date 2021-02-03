af8c36534899b4ca64af97ded65d19db0ed01fd2
Gama Aristondo <gamaliel@cs.ucsb.edu>
2021-02-02 17:29:51 -0800

    working on loop

--- swap_array.asm
+++ swap_array.asm
@@ -203,7 +203,11 @@ doSwap:
         #   y--
         # }
 
-        # TODO: fill in the code
+        li $t0, 0  # x variable equals $t0
+        li $t1, 10 # y variable equals $t1
+
+        addi $t0, $t0, 1
+        addi $t1, $t1, -1
 
         # do not remove this last line
         jr $ra

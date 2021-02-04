4b925a0356c0b3599384e8721d8b752ddd07c12b
Gama Aristondo <gamaliel@cs.ucsb.edu>
2021-02-03 16:29:56 -0800

    WIP

--- swap_array.asm
+++ swap_array.asm
@@ -205,9 +205,15 @@ doSwap:
 
         li $t0, 0  # x variable equals $t0
         li $t1, 10 # y variable equals $t1
+        li $t2, 5  # variable used to stop the loop
 
+doSwap_loop:
+
+
+        li $t0, 
         addi $t0, $t0, 1
         addi $t1, $t1, -1
+        bne $t0, $t2, doSwap_loop
 
         # do not remove this last line
         jr $ra
